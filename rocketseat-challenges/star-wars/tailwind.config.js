cb28a10b366ff533a0da45f00bef8d4ec5f53773
Rafael Thumaza <rassraza4@gmail.com>
2023-09-26 13:41:10 -0300

    feat: style aplication

--- rocketseat-challenges/star-wars/tailwind.config.js
+++ rocketseat-challenges/star-wars/tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "purple-base": "#ECE4EF",
+        "purple-text": "#D8CEDE",
+        "purple-secondary-text": "#9C83A7",
+        "purple-cards-titles": "#312D74",
+        "background-initial": "#030422",
+        "background-final": "#090114",
       },
     },
   },
   plugins: [],
-}
+};
